2f1a8184173bf161cd92646779c37a92b73fd388
TutosKen <tutos1620@gmail.com>
2020-11-27 17:14:24 -0500

    Vista modificar usuario

---
+++ AppDroid/AppDroid/Views/ModificarUsuarioPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppDroid.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ModificarUsuarioPage : ContentPage
+    {
+        public ModificarUsuarioPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnModificar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void BtnCancelar_Click(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+        private void BtnEliminarCuenta_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
