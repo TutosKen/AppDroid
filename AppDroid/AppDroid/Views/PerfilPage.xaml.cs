789f05cb8872b4b6efa52864c2c59c98ec03a8be
TutosKen <tutos1620@gmail.com>
2020-11-29 14:06:34 -0500

    Creada pagina de perfil de usuario

--- AppDroid/AppDroid/Views/PerfilPage.xaml.cs
+++ AppDroid/AppDroid/Views/PerfilPage.xaml.cs
@@ -17,9 +17,9 @@ namespace AppDroid.Views
             InitializeComponent();
         }
 
-        private void BtnModificar_Click(object sender, EventArgs e)
+        private async void BtnModificar_Click(object sender, EventArgs e)
         {
-
+            await Navigation.PushAsync(new ModificarUsuarioPage());
         }
 
         private void BtnEliminarCuenta_Click(object sender, EventArgs e)
